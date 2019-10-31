ab5cf928e4198c94ddc2fa76b479741cadffcc16
atimidguy <dumingcong0119@gmail.com>
2019-10-31 10:51:37 +0800

    Finished city-search-logic

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,18 +1,99 @@
 <template>
+<<<<<<< Updated upstream
   <div class="search">
     <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+=======
+  <div>
+    <div class="search">
+      <input
+        class="search-input"
+        type="text"
+        placeholder="输入城市名或拼音"
+        v-model="keyword"
+      />
+      <div class="search-result-wrapper" v-show="keyword">
+        <div class="search-result">
+          <div
+            class="result-item border-bottom"
+            v-for="(item, index) in filteredCities"
+            :key="index"
+          >
+            <!-- v-for="innerItem in item"
+            :key="innerItem" -->
+            {{ item.name }}
+          </div>
+          <div class="result-item" v-show="hasNoData">
+            没有找到匹配数据
+          </div>
+        </div>
+      </div>
+    </div>
+>>>>>>> Stashed changes
   </div>
 </template>
 
 <script>
 export default {
-  name: "CitySearch"
+  name: "CitySearch",
+  props: ["cities"],
+  data() {
+    return {
+      keyword: "",
+      filteredCities: [],
+      timer: null
+    };
+  },
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      if (!this.keyword) {
+        this.filteredCities = [];
+        return;
+      }
+      this.timer = setTimeout(() => {
+        let result = [];
+        // let tempRes = [];
+        for (let i in this.cities) {
+          // using Array.prototype.filter
+          //   tempRes = this.cities[i].filter(
+          //     value =>
+          //       value.spell.indexOf(this.keyword) > -1 ||
+          //       value.name.indexOf(this.keyword) > -1
+          //   );
+          //   result.push(...tempRes);
+
+          // using Array.prototype.forEach
+          this.cities[i].forEach(value => {
+            if (
+              value.spell.indexOf(this.keyword) > -1 ||
+              value.name.indexOf(this.keyword) > -1
+              // value.spell.startsWith(this.keyword) ||
+              // value.name.startsWith(this.keyword)
+            ) {
+              result.push(value);
+            }
+          });
+        }
+        this.filteredCities = result;
+      }, 100);
+    }
+  },
+  computed: {
+    hasNoData() {
+      return !this.filteredCities.length;
+    }
+  }
 };
 </script>
 
 <style lang="stylus" scoped>
 @import '~styles/variables.styl'
 
+.border-bottom
+  &:before
+    border-color #ccc
 .search
   height 0.72rem
   padding 0 0.1rem
@@ -27,4 +108,22 @@ export default {
     padding 0.1rem
     text-align center
     color #666
+  .search-result
+    max-height 6.3rem
+    overflow auto
+    position absolute
+    border-radius 0.06rem
+    box-shadow 1px 1px 3px #cfcfcf
+    width 86%
+    box-sizing border-box
+    top 1.54rem
+    left 7%
+    background-color #fdfdfd
+    text-align center
+    color #666
+    z-index 100
+    .result-item
+      font-size 0.28rem
+      line-height 0.62rem
+      height 0.62rem
 </style>
