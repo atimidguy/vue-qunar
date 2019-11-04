f9245c8faf4c7b2a6e113473d58142a5772e6321
atimidguy <dumingcong0119@gmail.com>
2019-11-04 19:25:14 +0800

    finished detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
     <div class="content">
-      <detail-header></detail-header>
+      <detail-list :categoryList="categoryList"></detail-list>
     </div>
   </div>
 </template>
@@ -10,11 +11,32 @@
 <script>
 import DetailBanner from "./components/Banner";
 import DetailHeader from "./components/Header";
+import DetailList from "./components/List";
 export default {
   name: "DefaultBanner",
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data() {
+    return {
+      categoryList: [
+        {
+          title: "成人票",
+          children: [
+            { title: "成人三馆联票" },
+            { title: "成人三馆联票 - 某一连锁店销售" }
+          ]
+        },
+        { title: "学生票" },
+        {
+          title: "儿童票",
+          children: [{ title: "儿童三馆联票 - 某一连锁店销售" }]
+        },
+        { title: "特惠票" }
+      ]
+    };
   }
 };
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="item" v-for="(item, index) in categoryList" :key="index">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>{{ item.title }}
+      </div>
+      <div class="item-children" v-if="item.children">
+        <detail-list :categoryList="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailList",
+  props: {
+    categoryList: Array
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.item-title
+  line-height 0.8rem
+  font-size 0.32rem
+  padding 0 0.2rem
+  .item-title-icon
+    position relative
+    left 0.06rem
+    top 0.06rem
+    display inline-block
+    width 0.36rem
+    height 0.36rem
+    background url('http://s.qunarzz.com/piao/image/touch/sight/detail.png') 0 -0.45rem no-repeat // s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+    margin-right 0.1rem
+    background-size 0.4rem 3rem
+.item-children
+  padding 0 0.2rem
+</style>
